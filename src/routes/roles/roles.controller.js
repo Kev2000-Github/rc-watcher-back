33748155df3843af9501ef1747b9816ddea3834a
chengkev <chengkev2000@gmail.com>
2023-09-04 15:18:10 -0400

    fix: refactor add level url query on get roles

--- src/routes/roles/roles.controller.js
+++ src/routes/roles/roles.controller.js
@@ -1,12 +1,16 @@
 
 const { controllerWrapper } = require('../../utils/common')
-const {Roles} = require('../../database/models')
+const {Roles, Sequelize} = require('../../database/models')
 const { paginate } = require('../../database/helper')
 const { responseData } = require('./helper')
+const { ROLES } = require('../../database/constants')
 
 module.exports.get_roles = controllerWrapper(async (req, res) => {
+    const { Op } = Sequelize
     const pagination = req.pagination
+    const level = req.query.level
     const opts = {...pagination}
+    if(level && level == 2) opts.where = {name: {[Op.ne]: ROLES.ADMIN}}
     let roles = await paginate(Roles, opts)
     roles.data = roles.data.map(role => responseData(role))
     res.json({...roles})
